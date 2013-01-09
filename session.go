c617ecf3f75d728adf07ada7d0fc5faa933e6c27
Lucas Liu <extraf@live.cn>
2013-01-09 17:52:59 +0800

    Change gorilla dependancy from code.google.com to github.com
    I don't want to use hg in MAC :-(

--- session.go
+++ session.go
@@ -1,41 +1,39 @@
 package goweb
 
 import (
-	"code.google.com/p/gorilla/sessions"
-	"net/http"
-	"reflect"
 	"encoding/json"
 	"errors"
+	"github.com/gorilla/sessions"
+	"net/http"
+	"reflect"
 )
 
 var store *sessions.CookieStore
 
-func EnableSession(){
+func EnableSession() {
 	store = sessions.NewCookieStore([]byte("非常保密"))
 }
 
-func MarshallInCookieSession(obj interface{},r *http.Request, w http.ResponseWriter){
+func MarshallInCookieSession(obj interface{}, r *http.Request, w http.ResponseWriter) {
 	typ := reflect.TypeOf(obj).String()
-	session, _ := store.Get(r,typ)
-	session.Values[typ],_ = json.Marshal(obj)
-	session.Save(r,w)
+	session, _ := store.Get(r, typ)
+	session.Values[typ], _ = json.Marshal(obj)
+	session.Save(r, w)
 }
 
-func UnmarshallInCookieSession(obj interface{},r *http.Request)error{
+func UnmarshallInCookieSession(obj interface{}, r *http.Request) error {
 	typ := reflect.TypeOf(obj).String()
-	session, _ := store.Get(r,typ)
+	session, _ := store.Get(r, typ)
 	res := session.Values[typ]
-	if res == nil{
+	if res == nil {
 		return errors.New("no such record")
 	}
-	return json.Unmarshal(res.([]byte),obj)
+	return json.Unmarshal(res.([]byte), obj)
 }
 
-
-func DeleteFromSession(obj interface{},r *http.Request, w http.ResponseWriter){
+func DeleteFromSession(obj interface{}, r *http.Request, w http.ResponseWriter) {
 	typ := reflect.TypeOf(obj).String()
-	session, _ := store.Get(r,typ)
-	delete(session.Values,typ)
-	session.Save(r,w)
+	session, _ := store.Get(r, typ)
+	delete(session.Values, typ)
+	session.Save(r, w)
 }
-
